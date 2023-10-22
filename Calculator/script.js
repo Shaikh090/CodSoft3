cb406e4b1c2934b2e5c55d6fbead8a627fe62f49
Shaikh Arsh <146738529+Shaikh090@users.noreply.github.com>
2023-10-22 18:41:25 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,26 @@
+let currentDisplay = '';
+
+function appendToDisplay(value) 
+{
+    currentDisplay += value;
+    document.getElementById('display').value = currentDisplay;
+}
+
+function clearDisplay() 
+{
+    currentDisplay = '';
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() 
+{
+    try 
+    {
+        currentDisplay = eval(currentDisplay);
+        document.getElementById('display').value = currentDisplay;
+    } 
+    catch (error) 
+    {
+        document.getElementById('display').value = 'Error'
+    }
+}
